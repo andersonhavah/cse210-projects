015196c07b29760a97bd31badf5f1d679203e68c
andersonhavah <havahanderson@gmail.com>
2024-04-25 04:06:34 +0000

    Finished prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Ask the user for their first and last name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Display the user's name to the console 
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
