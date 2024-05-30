bb6f9b81de9fee0e781e1dbdd2eff7ef28f2725c
andersonhavah <havahanderson@gmail.com>
2024-05-30 12:02:49 +0000

    Finished W04 Learning Activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Assignment
+        Assignment assignment = new Assignment("John Samuel", "Multiplication");
+        string summary = assignment.GetSummary();
+        Console.WriteLine($"{summary}\n");
+
+        // Math Assignment
+        MathAssignment mathAssignment= new MathAssignment("Jack Roberts", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine($"{mathAssignment.GetHomeworkList()}\n");
+
+        // Writing Assignment
+        WritingAssignment writingAssignment= new WritingAssignment("Johnson Lockhard", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+
     }
 }
