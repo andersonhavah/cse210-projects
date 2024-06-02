180913c57a712defb2e2d6aab918313d5f8b8c7e
andersonhavah <havahanderson@gmail.com>
2024-06-02 02:25:50 +0000

    ReflectingActivity Class completed - Week 04

---
+++ prove/Develop04/ReflectingActivity.cs
@@ -0,0 +1,85 @@
+
+public class ReflectingActivity : Activity
+{
+    // This is the class' attributes
+    private List<string> _prompts = new List<string>
+        {
+            "Think of a time when you stood up for someone else.",
+            "Think of a time when you did something really difficult.",
+            "Think of a time when you helped someone in need.",
+            "Think of a time when you did something truly selfless."
+        };
+    private List<string> _questions = new List<string>
+        {
+            "Why was this experience meaningful to you?",
+            "Have you ever done anything like this before?",
+            "How did you get started?",
+            "How did you feel when it was complete?",
+            "What made this time different than other times when you were not as successful?",
+            "What is your favorite thing about this experience?",
+            "What could you learn from this experience that applies to other situations?",
+            "What did you learn about yourself through this experience?",
+            "How can you keep this experience in mind in the future?",
+        };
+
+    // This is the constructor
+    public ReflectingActivity()
+    {
+        _name = "Reflection Activity";
+        _description = "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life."; 
+
+    }
+
+    public void Run()
+    {
+        DisplayStartingMessage();
+        ShowSpinner(3);
+
+        DisplayPrompt();
+        DisplayQuestions();
+
+        DisplayEndingMessage();
+    }
+
+    public string GetRandomPrompt()
+    {
+        Random rand = new Random();
+        int index = rand.Next(_prompts.Count);
+        string randomPrompt = $"{_prompts[index]}";
+        return randomPrompt;
+    }
+
+    public string GetRandomQuestion()
+    {
+        Random rand = new Random();
+        int index = rand.Next(_questions.Count);
+        string randomQuestion = $"Question: {_questions[index]}";
+        return randomQuestion;
+    }
+
+    public void DisplayPrompt()
+    {
+        Console.WriteLine("Consider the following prompt: \n");
+        Console.WriteLine($"--- {GetRandomPrompt()} ---");
+        Thread.Sleep(1000); // Pause for 1 seconds after each prompt
+        Console.WriteLine("\nWhen you have something in your mind, press enter to continue.");
+        Console.ReadKey();
+
+    }
+
+    public void DisplayQuestions()
+    {
+        Console.WriteLine("\nNow ponder on each of the following questions as they related to this experience.");
+        Console.Write($"You may begin in: ");
+        ShowCountDown(5);
+
+        Console.Clear();
+        foreach (string question in _questions)
+        {
+            Console.Write($"\n> {question}");
+            ShowSpinner(3);
+            Thread.Sleep(3000); // Pause for 3 seconds after each question
+        }
+    }
+
+}
