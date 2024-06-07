f48fa5416c7090d34748d9a5f5ad6cf9aab2e621
andersonhavah <havahanderson@gmail.com>
2024-06-07 02:54:14 +0000

    Customer Class Completed

---
+++ final/Foundation2/Customer.cs
@@ -0,0 +1,34 @@
+// This is the customer class for the online order program
+using System;
+
+public class Customer
+{
+    // Below are the member variables
+    private string _customerName;
+    private Address _customerAddress;
+
+    // This is the constructor
+    public Customer(string customerName, Address customerAddress)
+    {
+        _customerName = customerName;
+        _customerAddress = customerAddress;
+    }
+
+    // This function checks if the customer is in or out of USA
+    public bool IsCustomerInUSA()
+    {
+        return _customerAddress.IsAddressInUSA();
+    }
+
+    // This function returns the customer name
+    public string GetCustomerName()
+    {
+        return _customerName;
+    }
+
+    // This function returns the customer address
+    public string GetCustomerAddress()
+    {
+        return _customerAddress.GetFullAddressForm();
+    }
+}
