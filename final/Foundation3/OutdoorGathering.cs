2dc3271c1d17a7787feec0e893ce2ecc88895bd8
andersonhavah <havahanderson@gmail.com>
2024-06-07 17:01:25 +0000

    OutdoorGathering Class Completed

--- final/Foundation3/OutdoorGathering.cs
+++ final/Foundation3/OutdoorGathering.cs
@@ -16,18 +16,18 @@ public class OutdoorGathering : Event
     // This method returns the standard details of the Reception event
     public string GetStandardDetails()
     {
-        return $"Event Information: \n\tTitle: {GetTitle()}\n\tDescription: {GetDescription()}\n\tDate: {GetDate()}\n\tTime: {GetTime()}\n\tAddress: {GetAddress()}";
+        return $"Standard Details: \n\tTitle: {GetTitle()}\n\tDescription: {GetDescription()}\n\tDate: {GetDate()}\n\tTime: {GetTime()}\n\tAddress: {GetAddress()}";
     }
 
     // This method returns the full details of the Reception event
     public string GetFullDetails()
     {
-        return $"{GetStandardDetails()}\n\tWeather Forecast: {_weatherForecast}";
+        return $"Full Details: \n\tTitle: {GetTitle()}\n\tDescription: {GetDescription()}\n\tDate: {GetDate()}\n\tTime: {GetTime()}\n\tAddress: {GetAddress()}\n\tWeather Forecast: {_weatherForecast}";
     }
 
     // This method returns a short description of the Reception event
     public string GetShortDescription()
     {
-        return $"Event Type: {GetType().Name}\nTitle: {GetTitle()}\nDate: {GetDate()}";
+        return $"Short Description: \n\tEvent Type: {GetType().Name}\n\tTitle: {GetTitle()}\n\tDate: {GetDate()}";
     }
 }
