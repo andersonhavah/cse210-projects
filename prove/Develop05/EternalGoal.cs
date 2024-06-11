ffffc52d73615f72c7a1baea2adfe0f6f595586d
andersonhavah <havahanderson@gmail.com>
2024-06-11 13:39:42 +0000

    EternalGoal Class Completed

---
+++ prove/Develop05/EternalGoal.cs
@@ -0,0 +1,28 @@
+// This is the eternal goal class
+using System;
+
+
+public class EternalGoal : Goal
+{
+    // This is the constructor
+    public EternalGoal(string name, string description, int points) : base(name, description, points)
+    {
+
+    }
+
+    public override void RecordEvent()
+    {
+        // Eternal goals don't get marked as complete
+    }
+
+    public override bool IsComplete()
+    {
+        return false; // Eternal goals are never complete
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return $"Eternal Goal: {_shortName}, {_description}, {_points}";
+
+    }
+}
